0c48c61c3c532a984b72fd56c7d9311683c14d31
saurabh chuadhary <130744848+Saurabh1binrootlab@users.noreply.github.com>
2023-04-17 16:31:38 +0530

    animations
    some animations are add to the front page

--- javascript/main_page.js
+++ javascript/main_page.js
@@ -1,7 +1,7 @@
 // on scroll header transparent
 window.onscroll = function() {myFunction()};
 function myFunction() {
-if (document.documentElement.scrollTop > 800) {
+if (document.documentElement.scrollTop > 750) {
     document.getElementById("nav_bar").className = "on_scroll";
 } else {
     document.getElementById("nav_bar").className = "header_container";
